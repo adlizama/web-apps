ea477cabe6795aec1c1e5bfea27571c18c7a1292
Anthony Lizama <anthony.lizama@kellogg.northwestern.edu>
2024-02-08 03:14:04 +0000

    companies lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get "/tacos", :controller => "tacos", :action => "index"
   get "/dice", :controller => "dice", :action => "index"
+  
+  resources "companies"
+  resources "contacts"
+  #get "/companies", :controller => "companies", :action => "index"
 end
 
